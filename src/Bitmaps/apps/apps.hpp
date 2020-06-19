4a9405ce93ff60fa9dee8d8890824c869b872ab2
Filip Budiša <filip@circuitmess.com>
2020-06-19 16:29:07 +0200

    Update icons and add app bitmaps

---
+++ src/Bitmaps/apps/apps.hpp
@@ -0,0 +1,9 @@
+#ifndef CIRCUITWATCH_APPS_H
+#define CIRCUITWATCH_APPS_H
+
+#include "app_gesture.hpp"
+#include "app_playground.hpp"
+#include "app_programming.hpp"
+#include "app_settings.hpp"
+
+#endif //CIRCUITWATCH_APPS_H
