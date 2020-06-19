f68bb63dba474eb9b83c26e6b2a5da87b418109a
Filip Budiša <filip@circuitmess.com>
2020-06-19 16:31:25 +0200

    Add 3D demo app

---
+++ src/Apps/Playground/Playground.cpp
@@ -0,0 +1,45 @@
+#include "Playground.h"
+#include "../../CircuitWatch.h"
+#include "../../Components/Renderer.h"
+#include "Input/Input.h"
+#include "Update/UpdateManager.h"
+
+Playground* Playground::instance = nullptr;
+
+Playground::Playground(Display& display) : Context(display), renderer(Renderer::getInstance()){
+	instance = this;
+	pack();
+}
+
+void Playground::draw(){
+
+}
+
+void Playground::start(){
+	Input::getInstance()->setBtnPressCallback(BTN_N, [](){
+		if(instance == nullptr) return;
+		instance->pop();
+	});
+
+	UpdateManager::addListener(this);
+	renderer->start();
+}
+
+void Playground::stop(){
+	renderer->stop();
+	delay(20);
+	UpdateManager::removeListener(this);
+	Input::getInstance()->removeBtnPressCallback(BTN_N);
+}
+
+void Playground::update(uint micros){
+	float d = 0.001f;
+	euler.pitch += d; if(euler.pitch > M_PI) euler.pitch = -M_PI;
+	euler.yaw += d; if(euler.yaw > M_PI) euler.yaw = -M_PI;
+	euler.roll += d; if(euler.roll > M_PI) euler.roll = -M_PI;
+
+	glm::quat q(glm::vec3(euler.pitch, euler.yaw, euler.roll));
+	quatf rot = { q.w, q.x, q.y, q.z };
+
+	renderer->setRot(rot, euler);
+}
---
+++ src/Apps/Playground/Playground.h
@@ -0,0 +1,29 @@
+#ifndef CIRCUITWATCH_PLAYGROUND_H
+#define CIRCUITWATCH_PLAYGROUND_H
+
+#include <Update/UpdateListener.h>
+#include <Support/Context.h>
+#include <Motion/vec.hpp>
+
+class Renderer;
+
+class Playground : public Context, public UpdateListener {
+public:
+	Playground(Display& display);
+
+	void draw() override;
+	void update(uint micros) override;
+
+	void start() override;
+	void stop() override;
+
+private:
+	static Playground* instance;
+
+	Renderer* renderer = nullptr;
+	vec3f euler = { 0, 0, 0 };
+
+};
+
+
+#endif //CIRCUITWATCH_PLAYGROUND_H
